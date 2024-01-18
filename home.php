75a4344b7d1365fad3ed80e1f3d4b850c41df5c6
Core5570RYT <cepssetiadi20@gmail.com>
2024-01-18 08:44:04 +0700

    new update, forgot

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
     session_start();
     if (isset($_SESSION['username'])) {
         echo "Welcome, " . $_SESSION['username'] . "!";
+        header("Location: pages/accountcreation.html");
     } else {
         header("Location: login.php");
     }
